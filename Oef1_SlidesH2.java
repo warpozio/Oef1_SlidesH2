df92127f8f79aa211752efbb3e7754c7841b3448
warpozio <warpozio@users.noreply.github.com>
2016-05-07 18:12:58 +0200

    java file

---
+++ Oef1_SlidesH2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oef1_slidesh2;
+
+/**
+ *
+ * @author Ebu Bekir
+ */
+public class Oef1_SlidesH2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.printf("%s%n%n","Open de \"Folders\"\nD:\\temp\\OOPI");
+        System.out.printf("%s%n%n","Mijn naam is \"Jan Janssens\",\nIk volg de opleiding \'Toegepaste Informatica\'");
+        System.out.printf("%s%d%n","85 - 95 = ", 85 - 95);
+    }
+    
+}
